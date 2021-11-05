2c04864f90a8118210dd5172c9465e8ad8fe7c2b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-05 09:55:38 -0400

    demo spec for the video

---
+++ cypress/integration/demo-spec.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+it('uses full page object', () => {
+  const pages = {
+    visit() {
+      cy.visit('dist/index.html')
+
+      cy.contains('button[role=tab]', 'Page 1').as('page1')
+      cy.contains('button[role=tab]', 'Page 2').as('page2')
+
+      Object.defineProperties(window, {
+        page1: {
+          get() {
+            return cy.get('@page1')
+          },
+        },
+        page2: {
+          get() {
+            return cy.get('@page2')
+          },
+        },
+      })
+    },
+  }
+
+  pages.visit()
+  page1.should('have.attr', 'aria-selected', 'true')
+  page2.should('not.have.attr', 'aria-selected', 'true')
+
+  page2.click().should('have.attr', 'data-loading')
+
+  page2.should('have.attr', 'aria-selected', 'true')
+  page1.should('not.have.attr', 'aria-selected', 'true')
+
+  page2.should('not.have.attr', 'data-loading')
+})
